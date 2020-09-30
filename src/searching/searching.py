bdca06353d07e530cdcb2c8ce833dad43025702f
Khoa Tran <jacktran1799@gmaail.com>
2020-09-30 12:16:57 -0500

    finish with searching algorithm

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,21 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
+    if target in arr:
+        return arr.index(target)
+    else:
+        return -1
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
-
-    # Your code here
-
-
-    return -1  # not found
+    arr.sort()
+    min_value=0
+    max_value=(len(arr) - 1)
+    while max_value >= min_value:
+        middle=(min_value + max_value) // 2
+        if target == arr[middle]:
+            return middle
+        elif target < arr[middle]:
+            max_value= middle - 1
+        elif target > arr[middle]:
+            min_value= middle + 1
+    return -1
